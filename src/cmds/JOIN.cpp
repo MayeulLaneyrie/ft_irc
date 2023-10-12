58f40130f6d6328dfab19717321cdae4e364aaf0
Said HAMIZI <shamizi@paul-f3Br4s1.clusters.42paris.fr>
2023-10-12 16:07:39 +0200

    JOIN multichannel + multikey

--- src/cmds/JOIN.cpp
+++ src/cmds/JOIN.cpp
@@ -17,21 +17,57 @@ int User::_cmd_JOIN(Msg & cmd) // ----------------------------------------- JOIN
 	str_vec arg = cmd.payloadAsVector(2);
 	if (arg.size() < 1)
 		return (rpl(ERR_NEEDMOREPARAMS, "JOIN"));
-	Chan * channel = _serv->getChan(arg[0]);
-	if (!channel) {
-		channel = _serv->addChan(arg[0]);
-		channel->opMode(this, 1);
+	str_vec ChannelNames;
+	str_vec Key;
+	std::vector<str>::iterator it;
+	std::vector<str>::iterator keyIt;
+	while (!arg[0].empty())
+		ChannelNames.push_back(extract_first_word(arg[0], ','));
+	if (arg.size() == 2)
+		{
+			while (!arg[1].empty())
+				Key.push_back(extract_first_word(arg[1], ','));
+			keyIt = Key.begin();
+		}
+	for (it = ChannelNames.begin(); it != ChannelNames.end(); it++)
+	{
+		Chan *channel = _serv->getChan(*it);
+		if (!channel)
+		{
+			if (is_valid_name(*it) || is_name_chan(*it))
+			{
+				channel = _serv->addChan(*it);
+				channel->opMode(this, 1);
+			}
+		}
+		if (channel)
+		{
+			if (channel->getUser(_nick))
+				;
+			else if (channel->checkMode(MODE_I) && !channel->isInvited(this))
+				rpl(ERR_INVITEONLYCHAN, *it);
+			else if (channel->checkMode(MODE_K) && (arg.size() < 2 || !channel->checkPasswd(*keyIt)))
+				rpl(ERR_BADCHANNELKEY, *it);
+			else if (channel->checkMode(MODE_L) && channel->isFull())
+				(rpl(ERR_CHANNELISFULL, *it));
+			else
+			{
+				channel->addUser(this);
+				_chans[*it] = channel;
+				channel->chan_send(NULL, Msg(_nick, "JOIN", ":" + *it));
+				if (channel->getTopic() != "")
+					rpl(RPL_TOPIC, arg[0] + " :" + channel->getTopic());
+				_cmd_NAMES("", "", *it);
+			}
+			if (arg.size() == 2 && keyIt != Key.end())
+				keyIt++;
+		}
 	}
-	if (channel->getUser(_nick))
-		return (0);
-	if (channel->checkMode(MODE_I) && !channel->isInvited(this))
-		return (rpl(ERR_INVITEONLYCHAN, arg[0]));
-	if (channel->checkMode(MODE_K) && (arg.size() < 2 || !channel->checkPasswd(arg[1])))
-		return (rpl(ERR_BADCHANNELKEY, arg[0]));
-	if (channel->checkMode(MODE_L) && channel->isFull())
-		return (rpl(ERR_CHANNELISFULL, arg[0]));
-	channel->addUser(this);
-	_chans[arg[0]] = channel;
-	channel->chan_send(NULL, Msg(_nick, "JOIN", str(":") + arg[0]));
 	return (0);
 }
+//verif du nom du chan,
+//envoyer rpl topic
+//liste de key aussi
+//ajouter liste des user + @ si opetateur (RPL)
+//@ sur whois si OP
+//ajouter invite a la cmd
